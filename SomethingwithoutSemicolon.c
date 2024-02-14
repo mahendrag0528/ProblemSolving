28c034b0d9f8d4d02254aeaeef2429c8079c62f1
mahendrag0528.github.io <101779844+mahendrag0528@users.noreply.github.com>
2024-02-14 13:02:16 +0530

    Add files via upload

---
+++ SomethingwithoutSemicolon.c
@@ -0,0 +1,9 @@
+//write hello world without semicolon
+
+
+#include<stdio.h>
+
+int main(){
+if(printf("Hello World!")){}
+return 0;
+}
---
+++ withoutmain.c
@@ -0,0 +1,9 @@
+//write hello world without main function
+//using directives
+
+#include<stdio.h>
+#define fun main
+int fun(){
+printf("Hello World!");
+return 0;
+}
